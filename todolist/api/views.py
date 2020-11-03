28b9fa37577ac5123e132f4bf97147d91f3b0996
Matheus Amorim <matheus_ads@hotmail.com>
2020-11-03 17:58:03 -0300

    add views and routes

--- todolist/api/views.py
+++ todolist/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.viewsets import ModelViewSet
+from .serializers import UserSerializer, TaskSerializer
+from .models import TaskModel
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = TaskModel.objects.all()
+    serializer_class = TaskSerializer
--- todolist/urls.py
+++ todolist/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from todolist.api.views import UserViewSet, TaskViewSet
 
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'tasks', TaskViewSet)
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+
