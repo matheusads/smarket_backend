e838d9adac00ee1b0f8ae10c999a3e9db7b0d2c2
Matheus Amorim <matheus_ads@hotmail.com>
2020-11-03 17:55:05 -0300

    add task model

--- todolist/api/models.py
+++ todolist/api/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TaskModel(models.Model):
+    STATUS_CHOICE = [
+        ('C', 'Created'), ('P', 'In progress'), ('D', 'Done')
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.CharField(max_length=1000)
+    status = models.CharField(choices=STATUS_CHOICE, default='C', max_length=100)
+    user_id = models.ForeignKey(User, related_name='task', on_delete=models.SET_NULL, null=True)
